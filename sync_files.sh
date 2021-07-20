63d88bc3bcf44bc9e58ec2634ad9b8c28a98a88f
johnmcarter <jmcarter211@gmail.com>
2021-07-19 20:30:31 -0400

    edited sync script

--- sync_files.sh
+++ sync_files.sh
@@ -2,13 +2,15 @@
 
 # Author: John Carter
 # Created: 2021/07/04 17:57:02
-# Last modified: 2021/07/06 09:21:06
+# Last modified: 2021/07/19 20:30:16
 
 GRN=$'\e[1;32m'
 END=$'\e[0m'
 
 echo "[${GRN}INFO${END}] Copying data...."
+cd website
 git pull
+cd ..
 cp -r website/ /home/john/
 
 echo "[${GRN}INFO${END}] Restarting service...."
