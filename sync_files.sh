4a5a4eb74c4a85802687415fb3f94d9ae34c6129
johnmcarter <jmcarter211@gmail.com>
2021-07-19 20:44:27 -0400

    edited sync files script

--- sync_files.sh
+++ sync_files.sh
@@ -2,14 +2,15 @@
 
 # Author: John Carter
 # Created: 2021/07/04 17:57:02
-# Last modified: 2021/07/19 20:30:16
+# Last modified: 2021/07/19 20:44:03
 
 GRN=$'\e[1;32m'
 END=$'\e[0m'
 
-echo "[${GRN}INFO${END}] Copying data...."
+echo "[${GRN}INFO${END}] Running git pull...."
 cd website
 git pull
+echo "[${GRN}INFO${END}] Copying data...."
 cd ..
 cp -r website/ /home/john/
 
